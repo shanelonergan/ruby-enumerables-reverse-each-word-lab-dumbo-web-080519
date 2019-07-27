da82d234ae48c93f7a28b4a8e5d9815524ae9014
Shane Lonergan <sptlonergan@gmail.com>
2019-07-27 02:21:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,6 +9,6 @@
 def reverse_each_word(string)
   words = string.split
   reverse_words = []
-  words.collect { |words| reverse_words >> words.reverse! }
+  words.collect { |words| reverse_words << words.reverse! }
   words.join(" ")
 end
